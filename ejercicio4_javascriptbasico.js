5a7956dd5f4fd260c3a1b4b88f2afdd07af6aa20
Daniela Roxana Escobar <108234679+DaniZoom@users.noreply.github.com>
2023-02-08 17:32:13 -0300

    Add files via upload

---
+++ ejercicio4_javascriptbasico.js
@@ -0,0 +1,7 @@
+var alturaCm = 164;
+var alturaM = 1.64;
+var pesoKg = 69.590;
+var alturaRedondeo = Math.ceil(alturaM);
+var pesoRedondeo = Math.floor(pesoKg);
+var max = Number.MAX_VALUE + 1;
+var maximo = "el maximo valor que se puede obtener en Javascript + 1" == max + 1;
