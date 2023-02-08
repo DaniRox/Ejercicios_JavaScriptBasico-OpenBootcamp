0c50c1d09047e71d5289c3568ed3db4d93fb10cf
Daniela Roxana Escobar <108234679+DaniZoom@users.noreply.github.com>
2023-02-08 17:50:56 -0300

    Add files via upload

---
+++ ejercicio5_javascriptbasico.js
@@ -0,0 +1,20 @@
+var listaDeCompras = ["leche", "azucar", "harina", "huevos", "manteca"];
+listaDeCompras.push("Aceite de Girasol");
+listaDeCompras.pop();
+var misPelis = [
+    {
+        titulo,
+        director,
+        fecha
+    },
+    {
+        titulo,
+        director,
+        fecha
+    },
+    {
+        titulo,
+        director,
+        fecha
+    },
+]
