3494233ca636a273255418160eacdb3f627ee995
Daniela Roxana Escobar <108234679+DaniZoom@users.noreply.github.com>
2023-02-20 16:46:43 -0300

    Add files via upload

---
+++ fechas.js
@@ -0,0 +1,11 @@
+const hoy = new Date();
+
+const miNacimiento = new Date(1987, 01, 21);
+
+const esMasTarde = hoy > miNacimiento;
+
+const diaDeMiNacimiento = miNacimiento.getDate();
+
+const mesDeMiNacimiento = miNacimiento.getMonth();
+
+const anyoDeMiNacimiento = miNacimiento.getFullYear();
